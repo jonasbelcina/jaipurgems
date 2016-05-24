a6261c30dea76bd93da251aacf1eb5f672c464d2
Jonas Belcina <jonasbelcina10@gmail.com>
2016-05-24 10:16:38 +0400

    Create About Us template

---
+++ wp-content/themes/jaipurgems/template-about-us.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template name: About Us
+ *
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+
+
+<?php get_footer(); ?>
