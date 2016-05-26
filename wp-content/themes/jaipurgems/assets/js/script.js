024d5dc184443d550eadc95c9a1e11d939468624
Jonas Belcina <jonasbelcina10@gmail.com>
2016-05-26 18:25:36 +0400

    Create About us page

--- wp-content/themes/jaipurgems/assets/js/script.js
+++ wp-content/themes/jaipurgems/assets/js/script.js
@@ -10,6 +10,103 @@
 			autoplay: true
 		});
 
+        // About Us - History slider
+        var owl = $('.history-slider');
+
+        owl.on('initialized.owl.carousel', function( event ){
+            $('.history-slider .owl-item:nth-of-type(1)').addClass('current');
+        });
+
+        owl.owlCarousel({
+            items: 3,
+            nav: true,
+            navText: false,
+            // loop: true,
+            dots: false,
+            touchDrag  : false,
+            mouseDrag  : false,
+            responsive : {
+                991 : {
+                    items : 3,
+                    onResized: removeLast
+                },
+                767: {
+                    items: 2,
+                },
+                0 : {
+                    items : 1,
+                }
+            },
+            // animateOut: 'fadeOut',
+            // autoplay: true
+            // afterInit: addCurrent
+        });
+
+        $('.custom-next').on('click', function() {
+            var current = $('.owl-item.current');
+            var currentYear = $('.history-year .current');
+
+            if(current.next('.owl-item').length) {
+                if(!current.next('.owl-item').hasClass('active')) {
+                    owl.trigger('next.owl.carousel');
+                }
+
+                current.removeClass('current');
+                // current.find('.history-item').css('opacity', '1');
+                // current.find('.history-item').css('opacity', '0.4');
+                current.next('.owl-item').addClass('current');
+
+                currentYear.removeClass('current');
+                currentYear.next('div').addClass('current');
+            }
+        });
+
+        $('.custom-prev').on('click', function() {
+            var current = $('.owl-item.current');
+            var currentYear = $('.history-year .current');
+
+            if(current.prev('.owl-item').length) {
+                if(!current.prev('.owl-item').hasClass('active')) {
+                    owl.trigger('prev.owl.carousel');
+                }
+
+                current.removeClass('current');
+                // current.find('.history-item').css('opacity', '0.4');
+                current.prev('.owl-item').addClass('current');
+
+                currentYear.removeClass('current');
+                currentYear.prev('div').addClass('current');
+            }
+        });
+
+        function removeLast() {
+            $('.history-slider .owl-item:nth-of-type(4)').removeClass('active');
+            console.log('removed');
+        }
+
+        removeLast();
+
+        // About Us - Expertise slider
+        $('.about-expertise-slider').owlCarousel({
+            items: 1,
+            nav: true,
+            navText: true,
+            // loop: true,
+            // animateOut: 'fadeOut',
+            autoplay: true
+        });
+
+        // About Us - Latest Collection slider
+        $('.latest-slider').owlCarousel({
+            items: 4,
+            nav: true,
+            navText: true,
+            loop: true,
+            dots: false
+            // animateOut: 'fadeOut',
+            // autoplay: true
+        });
+
 		// main nav mobile dropdown
         $('.dropdown-toggle span').on('click', function() {
             var ww = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
--- wp-content/themes/jaipurgems/template-about-us.php
+++ wp-content/themes/jaipurgems/template-about-us.php
@@ -10,6 +10,352 @@
 
 get_header(); ?>
 
+<div class="container">
+	<nav class="about-nav">
+		<ul>
+			<li><a href="#">The House</a></li>
+			<li><a href="#">History &amp; Heritage</a></li>
+			<li><a href="#">Our Expertise</a></li>
+			<li><a href="#">Philantrophy</a></li>
+			<li><a href="#">Product Line</a></li>
+		</ul>
+	</nav>
+</div>
 
+
+<section class="house-jaipur">
+	<div class="container">
+		<h2>The House of Jaipur Gems</h2>
+		<h3>Forever Now</h3>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, uis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+		<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+	</div>
+</section>
+
+<section class="owner">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6">
+				<h2 class="about-section-header">From the Owner</h2>
+				<div class="row">
+					<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/owner-video.jpg" />
+				</div>
+			</div>
+
+			<div class="col-md-6 col-sm-6">
+				<h2 class="about-section-header">Our Philosophy</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, uis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="heritage">
+	<div class="container">
+		<h2 class="about-section-header">Heritage &amp; History</h2>
+		<h3>Explore a rich heritage filled with Legendary jewellery &amp; Milestones</h3>
+		<div class="row">
+			<div class="history-wrap">
+				<div class="history-slider">
+					<div class="history-item">
+						<h4>1960 - The Beginning</h4>
+						<div class="content">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+						</div>
+						<div class="content-year">
+							1960
+						</div>
+					</div>
+
+					<div class="history-item">
+						<h4>1965 - First Collection</h4>
+						<div class="content">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/history2.jpg" />
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+						</div>
+						<div class="content-year">
+							1965
+						</div>
+					</div>
+
+					<div class="history-item">
+						<h4>1970 - Diamonds Collection</h4>
+						<div class="content">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/history3.jpg" />
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</p>
+						</div>
+						<div class="content-year">
+							1970
+						</div>
+					</div>
+
+					<div class="history-item">
+						<h4>1979 - Latest Collection</h4>
+						<div class="content">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+						</div>
+						<div class="content-year">
+							1979
+						</div>
+					</div>
+				</div>
+
+				<div class="custom-nav">
+					<div class="custom-prev"></div>
+					<div class="custom-next"></div>
+				</div>
+			</div>
+
+			<div class="history-year">
+				<div class="current"><span>1960</span></div>
+				<div><span>1965</span></div>
+				<div><span>1970</span></div>
+				<div><span>1979</span></div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="about-expertise">
+	<div class="container">
+		<h2 class="about-section-header">Our Expertise</h2>
+		<div class='about-expertise-slider'>
+			<div class="about-expertise-slide">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/about-expertise1.jpg" />
+				<div class="about-expertise-content">
+					<h3>Signature</h3>
+					<p>Two years ago, Mr. Siddhartha Sacheti curated a collection called the Dal Lake. Inspired by the beauty of Kashmir, it included delicate motifs like the lotus and the peacock. The latter, in particular, became an instant success, and has turned into a key element of the brand’s signature style. It features extensively in the jewellery designs of Jaipur Gems.</p>
+				</div>
+			</div>
+
+			<div class="about-expertise-slide">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/about-expertise1.jpg" />
+				<div class="about-expertise-content">
+					<h3>Excepteur</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="philantrophy">
+	<div class="container">
+		<h2 class="about-section-header">Philanthrophy</h2>
+		<h3>Jaipur Gems Charitable Trust</h3>
+		<div class="row">
+			<div class="col-md-3 col-sm-4 ph-tile">
+				<div class="row">
+					<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/p1.jpg" />
+					<div class="content">
+						<p>Jaipur Showcase 2016</p>
+						<a href="#">View Gallery</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-sm-4 ph-tile">
+				<div class="row">
+					<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/p2.jpg" />
+					<div class="content">
+						<p>Jaipur Showcase 2016</p>
+						<a href="#">View Gallery</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-sm-4 ph-tile">
+				<div class="row">
+					<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/p3.jpg" />
+					<div class="content">
+						<p>Jaipur Showcase 2016</p>
+						<a href="#">View Gallery</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-sm-4 ph-tile">
+				<div class="row">
+					<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/p4.jpg" />
+					<div class="content">
+						<p>Jaipur Showcase 2016</p>
+						<a href="#">View Gallery</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-sm-4 ph-tile">
+				<div class="row">
+					<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/p5.jpg" />
+					<div class="content">
+						<p>Jaipur Showcase 2016</p>
+						<a href="#">View Gallery</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-sm-4 ph-tile">
+				<div class="row">
+					<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/p6.jpg" />
+					<div class="content">
+						<p>Jaipur Showcase 2016</p>
+						<a href="#">View Gallery</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-sm-4 ph-tile">
+				<div class="row">
+					<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/p7.jpg" />
+					<div class="content">
+						<p>Jaipur Showcase 2016</p>
+						<a href="#">View Gallery</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3 col-sm-4 ph-tile">
+				<div class="row">
+					<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/p8.jpg" />
+					<div class="content">
+						<p>Jaipur Showcase 2016</p>
+						<a href="#">View Gallery</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="product-line">
+	<div class="container">
+		<h2 class="about-section-header">Product Line</h2>
+		<div class="row">
+			<div class="col-md-3">
+				<div class="row">
+					<ul class="nav nav-tabs" role="tablist">
+					    <li role="presentation" class="active"><a href="#materials" aria-controls="materials" role="tab" data-toggle="tab">Our Materials</a></li>
+					    <li role="presentation"><a href="#stones" aria-controls="stones" role="tab" data-toggle="tab">Precious Stones</a></li>
+					    <li role="presentation"><a href="#certifications" aria-controls="certifications" role="tab" data-toggle="tab">Certifications</a></li>
+				  	</ul>
+			  	</div>
+			</div>
+
+			<div class="col-md-9">
+				<div class="row">
+					<div class="tab-content">
+					    <div role="tabpanel" class="tab-pane fade in active" id="materials">
+					    	<div class="col-xs-7">
+					    		<p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut</strong></p>
+					    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+					    	</div>
+
+					    	<div class="col-xs-5">
+					    		<div class="row">
+					    			<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/pl1.jpg" />
+					    		</div>
+					    	</div>
+					    </div>
+					    <div role="tabpanel" class="tab-pane fade in" id="stones">
+					    	<div class="col-xs-7">
+					    		<p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut</strong></p>
+					    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+					    	</div>
+
+					    	<div class="col-xs-5">
+					    		<div class="row">
+					    			<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/pl1.jpg" />
+					    		</div>
+					    	</div>
+					    </div>
+					    <div role="tabpanel" class="tab-pane fade in" id="certifications">
+					    	<div class="col-xs-7">
+					    		<p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut</strong></p>
+					    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
+					    	</div>
+
+					    	<div class="col-xs-5">
+					    		<div class="row">
+					    			<img class="img-responsive" src="<?php echo get_template_directory_uri(); ?>/assets/images/pl1.jpg" />
+					    		</div>
+					    	</div>
+					    </div>
+				  	</div>
+			  	</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="latest-collection">
+	<div class="container">
+		<h2 class="about-section-header">Latest Collection</h2>
+		<div class="row">
+			<div class="latest-slider">
+				<div class="latest-item">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/product1.jpg" />
+					<div class="content">
+						<h3>The Superstar</h3>
+						<a href="#">View Products</a>
+					</div>
+				</div>
+
+				<div class="latest-item">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/product2.jpg" />
+					<div class="content">
+						<h3>The Superstar</h3>
+						<a href="#">View Products</a>
+					</div>
+				</div>
+
+				<div class="latest-item">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/product3.jpg" />
+					<div class="content">
+						<h3>The Superstar</h3>
+						<a href="#">View Products</a>
+					</div>
+				</div>
+
+				<div class="latest-item">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/product4.jpg" />
+					<div class="content">
+						<h3>The Superstar</h3>
+						<a href="#">View Products</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 
 <?php get_footer(); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
