74182913cbd3192f99550611f28ca167ee851b68
Jonas Belcina <jonasbelcina10@gmail.com>
2016-06-06 16:11:05 +0400

    Add woocommerce support

--- wp-content/themes/jaipurgems/functions.php
+++ wp-content/themes/jaipurgems/functions.php
@@ -615,20 +615,6 @@ function woo_redirect_to_checkout() {
 	return $checkout_url;
 }
 
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-
-add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
-
-function my_theme_wrapper_start() {
-  echo '<section id="main">';
-}
-
-function my_theme_wrapper_end() {
-  echo '</section>';
-}
-
 // Display xx products per page. Goes in functions.php
 add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 14;' ), 20 );
 
