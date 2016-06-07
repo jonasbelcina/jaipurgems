79b5c91ccc5f0c9937063f534e7532e096121be2
Jonas Belcina <jonasbelcina10@gmail.com>
2016-06-07 14:42:56 +0400

    Add Appointment form popup

--- wp-content/themes/jaipurgems/footer.php
+++ wp-content/themes/jaipurgems/footer.php
@@ -12,130 +12,145 @@
 
 	<footer>
 		<div class="footer-top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 col-sm-6">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="row">
-                                    <label>Join for newsletter</label>
-                                </div>
-                            </div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-sm-6">
+						<div class="row">
+							<div class="col-md-4">
+								<div class="row">
+									<label>Join for newsletter</label>
+								</div>
+							</div>
 
-                            <div class="col-md-8">
-                                <div class="row">
-                                    <form>
-                                        <div class="col-xs-9">
-                                            <div class="row">
-                                                <input type="email" placeholder="E-MAIL"></input>
-                                            </div>
-                                        </div>
+							<div class="col-md-8">
+								<div class="row">
+									<form>
+										<div class="col-xs-9">
+											<div class="row">
+												<input type="email" placeholder="E-MAIL"></input>
+											</div>
+										</div>
 
-                                        <div class="col-xs-3">
-                                            <div class="row">
-                                                <button type="submit">Sign In</button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+										<div class="col-xs-3">
+											<div class="row">
+												<button type="submit">Sign In</button>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+					</div>
 
-                    <div class="col-md-4 col-sm-4 col-md-offset-2 col-sm-offset-2 footer-social">
-                        <div class="row">
-                            <label>Social Media</label>
+					<div class="col-md-4 col-sm-4 col-md-offset-2 col-sm-offset-2 footer-social">
+						<div class="row">
+							<label>Social Media</label>
 
-                            <a href="<?php the_field('facebook' , 'options'); ?>" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                            <a href="<?php the_field('twitter' , 'options'); ?>" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                            <a href="<?php the_field('google_plus' , 'options'); ?>" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                            <a href="<?php the_field('pinterest' , 'options'); ?>" target="_blank"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
-                            <a href="<?php the_field('youtube' , 'options'); ?>" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+							<a href="<?php the_field('facebook' , 'options'); ?>" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+							<a href="<?php the_field('twitter' , 'options'); ?>" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+							<a href="<?php the_field('google_plus' , 'options'); ?>" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+							<a href="<?php the_field('pinterest' , 'options'); ?>" target="_blank"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+							<a href="<?php the_field('youtube' , 'options'); ?>" target="_blank"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-        <div class="footer-mid">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-8 col-sm-8 footer-menu">
-                        <div class="row">
-                            <div class="col-md-3 col-sm-6">
-                                <div class="row">
-                                    <h3>Maison <span class="glyphicon glyphicon-plus"></span></h3>
-                                    <ul>
-                                        <li><a href="#">Our Legacy</a></li>
-                                        <li><a href="#">Philanthropy</a></li>
-                                        <li><a href="#">Product Line</a></li>
-                                        <li><a href="#">News</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+		<div class="footer-mid">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-sm-8 footer-menu">
+						<div class="row">
+							<div class="col-md-3 col-sm-6">
+								<div class="row">
+									<h3>Maison <span class="glyphicon glyphicon-plus"></span></h3>
+									<ul>
+										<li><a href="#">Our Legacy</a></li>
+										<li><a href="#">Philanthropy</a></li>
+										<li><a href="#">Product Line</a></li>
+										<li><a href="#">News</a></li>
+									</ul>
+								</div>
+							</div>
 
-                            <div class="col-md-3 col-sm-6">
-                                <div class="row">
-                                    <h3>Shop Now <span class="glyphicon glyphicon-plus"></span></h3>
-                                    <ul>
-                                        <li><a href="#">Bangles</a></li>
-                                        <li><a href="#">Earrings</a></li>
-                                        <li><a href="#">Necklaces</a></li>
-                                        <li><a href="#">Our Collections</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+							<div class="col-md-3 col-sm-6">
+								<div class="row">
+									<h3>Shop Now <span class="glyphicon glyphicon-plus"></span></h3>
+									<ul>
+										<li><a href="#">Bangles</a></li>
+										<li><a href="#">Earrings</a></li>
+										<li><a href="#">Necklaces</a></li>
+										<li><a href="#">Our Collections</a></li>
+									</ul>
+								</div>
+							</div>
 
-                            <div class="col-md-3 col-sm-6">
-                                <div class="row">
-                                    <h3>My Account <span class="glyphicon glyphicon-plus"></span></h3>
-                                    <ul>
-                                        <li><a href="#">Sign In / Sign Up</a></li>
-                                        <li><a href="#">FAQ</a></li>
-                                        <li><a href="#">Privacy Policy</a></li>
-                                        <li><a href="#">Shipping &amp; Returns</a></li>
-                                    </ul>
-                                </div>
-                            </div>
+							<div class="col-md-3 col-sm-6">
+								<div class="row">
+									<h3>My Account <span class="glyphicon glyphicon-plus"></span></h3>
+									<ul>
+										<li><a href="#">Sign In / Sign Up</a></li>
+										<li><a href="#">FAQ</a></li>
+										<li><a href="#">Privacy Policy</a></li>
+										<li><a href="#">Shipping &amp; Returns</a></li>
+									</ul>
+								</div>
+							</div>
 
-                            <div class="col-md-3 col-sm-6 locator">
-                                <div class="row">
-                                    <a href="#">Store Locator</a>
-                                    <ul>
-                                        <li><a href="#">Contact via Email</a></li>
-                                        <li><a href="#">Make an Appointment</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+							<div class="col-md-3 col-sm-6 locator">
+								<div class="row">
+									<a href="#">Store Locator</a>
+									<ul>
+										<li><a href="#">Contact via Email</a></li>
+										<li><a href="#" data-toggle="modal" data-target="#appointmentForm">Make an Appointment</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
 
-                    <div class="col-md-3 col-md-offset-1 col-sm-4 footer-logo">
-                        <div class="row">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-footer.png" alt="Jaipur Gems" />
-                            <div class="footer-copy">&copy;2016 . All rights reserved.</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+					<div class="col-md-3 col-md-offset-1 col-sm-4 footer-logo">
+						<div class="row">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-footer.png" alt="Jaipur Gems" />
+							<div class="footer-copy">&copy;2016 . All rights reserved.</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 
-        <div class="footer-bottom">
-            <div class="container">
-                <div class="row">
-                    <div class="footer-cc">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/cc.png" alt="Payment Options" />
-                    </div>
+		<div class="footer-bottom">
+			<div class="container">
+				<div class="row">
+					<div class="footer-cc">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/cc.png" alt="Payment Options" />
+					</div>
 
-                    <div class="footer-heych">
-                        Powered by <a href="http://heych.com/" target="_blank">Heych</a>
-                    </div>
-                </div>
-            </div>
-        </div>
+					<div class="footer-heych">
+						Powered by <a href="http://heych.com/" target="_blank">Heych</a>
+					</div>
+				</div>
+			</div>
+		</div>
 
-        <a href="#top" class="cd-top"><span class="glyphicon glyphicon-chevron-up"></span></a>
+		<a href="#top" class="cd-top"><span class="glyphicon glyphicon-chevron-up"></span></a>
 	</footer>
+
+	<!-- Modal -->
+	<div class="modal fade" id="appointmentForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Make an Appointment</h4>
+				</div>
+				<div class="modal-body">
+					<?php echo do_shortcode('[contact-form-7 id="107" title="Make an Appointment"]'); ?>
+				</div>
+			</div>
+		</div>
+	</div>
 
 <?php wp_footer(); ?>
 </body>
