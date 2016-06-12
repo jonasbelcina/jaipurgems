aed2ea6ca95fa042a1ac1f4d9688fbbc712c6027
Jonas Belcina <jonasbelcina10@gmail.com>
2016-06-12 14:20:37 +0400

    Create Collections custom taxonomy

--- wp-content/themes/jaipurgems/functions.php
+++ wp-content/themes/jaipurgems/functions.php
@@ -938,6 +938,43 @@ add_action( 'init', 'jg_media_init' );
 // }
 // add_action('after_theme_switch', 'custom_flush_rules');
 
+/* Extra taxonomy for Collections */
+add_action( 'init', 'custom_taxonomy_Item' , 0 );
+function custom_taxonomy_Item()  {
+    $labels = array(
+        'name'                       => 'Collections',
+        'singular_name'              => 'Collection',
+        'menu_name'                  => 'Collections',
+        'all_items'                  => 'All Collections',
+        'parent_item'                => 'Parent Collection',
+        'parent_item_colon'          => 'Parent Collection:',
+        'new_item_name'              => 'New Collection Name',
+        'add_new_item'               => 'Add New Collection',
+        'edit_item'                  => 'Edit Collection',
+        'update_item'                => 'Update Collection',
+        'separate_items_with_commas' => 'Separate Collection with commas',
+        'search_items'               => 'Search Collections',
+        'add_or_remove_items'        => 'Add or remove Collections',
+        'choose_from_most_used'      => 'Choose from the most used Collections',
+    );
+    $args = array(
+        'labels'                     => $labels,
+        'hierarchical'               => true,
+        'public'                     => true,
+        'show_ui'                    => true,
+        'show_admin_column'          => true,
+        'show_in_nav_menus'          => true,
+        'show_tagcloud'              => false,
+        'rewrite'           => array( 'slug' => 'collection' , 'with_front' => true ),
+    );
+
+
+    register_taxonomy( 'collection',
+            apply_filters( 'woocommerce_taxonomy_objects_product_cat', array( 'product' ) ),
+            apply_filters( 'woocommerce_taxonomy_args_product_cat', $args) 
+        );
+    register_taxonomy_for_object_type( 'collection', 'product' );
+}
 
 
 
