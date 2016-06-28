4b4a2cc75b9a206b0d57351798cf7d1782c5d0bf
Jonas Belcina <jonasbelcina10@gmail.com>
2016-06-28 12:49:58 +0400

    Move quantity input to right for non bangles

--- wp-content/themes/jaipurgems/woocommerce/global/quantity-input.php
+++ wp-content/themes/jaipurgems/woocommerce/global/quantity-input.php
@@ -28,7 +28,7 @@ foreach ($terms as $term) {
     }
 }
 ?>
-<div class="quantity input-group">
+<div class="quantity input-group <?php if($product_cat_id != 7) { ?>not-bangle<?php } ?>">
 	<div class="label">Qty:</div>
 	<input type="number" step="<?php echo esc_attr( $step ); ?>" min="<?php echo esc_attr( $min_value ); ?>" max="<?php echo esc_attr( $max_value ); ?>" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" class="input-text qty text" size="4" />
 </div>
