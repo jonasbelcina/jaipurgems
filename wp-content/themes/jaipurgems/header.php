b3cd0e19d0c4d26b99cc984559e5c8ad62be547a
Jonas Belcina <jonasbelcina10@gmail.com>
2016-06-29 10:32:31 +0400

    Disable input zoom on focus for ios

--- wp-content/themes/jaipurgems/header.php
+++ wp-content/themes/jaipurgems/header.php
@@ -35,7 +35,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
