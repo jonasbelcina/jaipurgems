641736d44f1f7dad6490d9daa2013a84eacd2b69
Jonas Belcina <jonasbelcina10@gmail.com>
2016-07-03 13:25:48 +0400

    log session variables for test

--- wp-content/themes/jaipurgems/header.php
+++ wp-content/themes/jaipurgems/header.php
@@ -29,10 +29,10 @@
 		// setcookie('location', $location);
 
 		// server should keep session data for AT LEAST 1 hour
-		ini_set('session.gc_maxlifetime', 3600);
+		// ini_set('session.gc_maxlifetime', 3600);
 
 		// each client should remember their session id for EXACTLY 1 hour
-		session_set_cookie_params(3600);
+		// session_set_cookie_params(3600);
 
 		session_start(); // ready to go!
 	}
