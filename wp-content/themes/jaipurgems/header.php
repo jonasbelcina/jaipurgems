92d5d0d46c6faed860a35f8436cb2c5a28d696d2
Jonas Belcina <jonasbelcina10@gmail.com>
2016-07-03 11:27:42 +0400

    Update session timeout

--- wp-content/themes/jaipurgems/header.php
+++ wp-content/themes/jaipurgems/header.php
@@ -28,18 +28,13 @@
 		setcookie('country_code' , $country_code);
 		// setcookie('location', $location);
 
+		// server should keep session data for AT LEAST 1 hour
+		ini_set('session.gc_maxlifetime', 3600);
+
+		// each client should remember their session id for EXACTLY 1 hour
+		session_set_cookie_params(3600);
+
 		session_start(); // ready to go!
-
-		$now = time();
-		if (isset($_SESSION['discard_after']) && $now > $_SESSION['discard_after']) {
-		    // this session has worn out its welcome; kill it and start a brand new one
-		    session_unset();
-		    session_destroy();
-		    session_start();
-		}
-
-		// either new or old, it should live at most for another hour
-		$_SESSION['discard_after'] = $now + 36000; // 10 hrs
 	}
 
 ?><!DOCTYPE html>
