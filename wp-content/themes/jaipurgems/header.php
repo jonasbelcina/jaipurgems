09d1e427792dba584d7848e61e55496931498462
Jonas Belcina <jonasbelcina10@gmail.com>
2016-07-03 10:33:03 +0400

    Increase session timeout

--- wp-content/themes/jaipurgems/header.php
+++ wp-content/themes/jaipurgems/header.php
@@ -28,7 +28,18 @@
 		setcookie('country_code' , $country_code);
 		// setcookie('location', $location);
 
-		session_start();
+		session_start(); // ready to go!
+
+		$now = time();
+		if (isset($_SESSION['discard_after']) && $now > $_SESSION['discard_after']) {
+		    // this session has worn out its welcome; kill it and start a brand new one
+		    session_unset();
+		    session_destroy();
+		    session_start();
+		}
+
+		// either new or old, it should live at most for another hour
+		$_SESSION['discard_after'] = $now + 36000; // 10 hrs
 	}
 
 ?><!DOCTYPE html>
