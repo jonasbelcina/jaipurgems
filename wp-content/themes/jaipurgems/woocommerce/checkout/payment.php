091d0c5c7f1a7d62ed3d391fc9bbf5f94de6b705
Jonas Belcina <jonasbelcina10@gmail.com>
2016-07-05 15:14:09 +0400

    Add tag manager onclick event for Place Order

--- wp-content/themes/jaipurgems/woocommerce/checkout/payment.php
+++ wp-content/themes/jaipurgems/woocommerce/checkout/payment.php
@@ -46,7 +46,7 @@ if ( ! is_ajax() ) {
 
 		<?php do_action( 'woocommerce_review_order_before_submit' ); ?>
 
-		<?php echo apply_filters( 'woocommerce_order_button_html', '<input type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '" onclick="dataLayer.push({"event": "GAEvent", "eventCategory": "Place Order", "eventAction": "Order Success", "eventLabel": undefined, "eventValue": undefined});" />' ); ?>
+		<?php echo apply_filters( 'woocommerce_order_button_html', '<input type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '" onclick="dataLayer.push({\'event\': \'GAEvent\', \'eventCategory\': \'Place Order\', \'eventAction\': \'Order Success\', \'eventLabel\': undefined, \'eventValue\': undefined});" />' ); ?>
 
 		<?php do_action( 'woocommerce_review_order_after_submit' ); ?>
 
