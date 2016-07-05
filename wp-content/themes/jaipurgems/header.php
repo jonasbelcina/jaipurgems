a9d0fa1bfca39c992ac9531918c47e2e9bc3d2fa
Jonas Belcina <jonasbelcina10@gmail.com>
2016-07-05 11:03:50 +0400

    Add google tag manager

--- wp-content/themes/jaipurgems/header.php
+++ wp-content/themes/jaipurgems/header.php
@@ -71,17 +71,6 @@
 	<?php endif; ?>
 
 	<?php wp_head(); ?>
-
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-79596636-3', 'auto');
-	  ga('send', 'pageview');
-
-	</script>
 </head>
 
 <?php 
@@ -109,6 +98,16 @@ if(!isset($_SESSION['selected_country'])) {
 ?>
 
 <body <?php body_class($addtl_class); ?>>
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WJLSK8"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WJLSK8');</script>
+	<!-- End Google Tag Manager -->
+	
 	<div class="body-wrap">
 
 	<div class="page-loader">
